a7630e65241be406d36165953692d57dad055ac2
KattarinaOsadchenko <148332826+KattarinaOsadchenko@users.noreply.github.com>
2023-12-16 22:06:54 +0200

    HW 2a completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+// Task #1
+let admin = "John";
+let name = "John";
+
+alert(admin);
+
+// Task #2
+let planet = 'Earth';
+let visitorName = 'Visitor_name';
+
+// Task #3
+let myVariable = '10' + 10;
+console.log(typeof myVariable);
+console.log(myVariable);
