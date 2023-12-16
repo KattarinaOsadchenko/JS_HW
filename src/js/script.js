617f010d985da5b28a3eec32ccb01fe596935a45
KattarinaOsadchenko <148332826+KattarinaOsadchenko@users.noreply.github.com>
2023-12-16 23:39:37 +0200

    HW 3 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let number1 = prompt('Enter first number');
+let number2 = prompt('Enter second number');
+number1 = Number (number1);
+number2 = Number (number2);
+const sum = number1 + number2;
+console.log('storage:', sum);
+const difference = number1 - number2;
+console.log('difference:', difference);
+const product = number1 * number2;
+console.log('product:', product);
+const quotient = number1 / number2;
+console.log('quotient:', quotient);
