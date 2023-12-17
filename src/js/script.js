7d5f778210c5bb7dcb5d41d2914eb019a898cfdf
KattarinaOsadchenko <148332826+KattarinaOsadchenko@users.noreply.github.com>
2023-12-17 21:13:26 +0200

    HW 3 update

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,16 @@
 'use strict';
 // Task #1
-let admin = "John";
-let name = "John";
+const admin = "John";
+const name = "John";
 
 alert(admin);
 
 // Task #2
-let planet = 'Earth';
-let visitorName = prompt('Enter your name');
+const planet = 'Earth';
+const visitorName = prompt('Enter your name');
 alert('Hello,' + visitorName + '! How are you?');
 
 // Task #3
-let myVariable = '10' + 10;
+const myVariable = '10' + 10;
 console.log(typeof myVariable);
 console.log(myVariable);
