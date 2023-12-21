2cc477f1db7b687633c15b4a8c0b2e24e3e2a851
KattarinaOsadchenko <148332826+KattarinaOsadchenko@users.noreply.github.com>
2023-12-21 10:30:27 +0200

    HW 4 update

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+// Task 1
+let userName = prompt('Enter your name');
+let day = prompt('Enter what day it is');
+let mood = prompt('Enter what you are today');
+
+let result= userName + day + mood;
+
+console.log('result', result)
+
+// Task 2
+
