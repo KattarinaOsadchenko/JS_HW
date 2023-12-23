03f713356267474046c00ca66115b4eff2984c4f
KattarinaOsadchenko <148332826+KattarinaOsadchenko@users.noreply.github.com>
2023-12-24 00:51:20 +0200

    HW 4 done

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,12 @@ console.log('result', result)
 
 // Task 2
 
+const number = 12345;
+const numberString = number.toString();
+
+const firstChar = numberString [0];
+const secondChar = numberString [1];
+const thirdChar = numberString [2];
+const fourthChar = numberString [3];
+const lastChar = numberString [numberString.length - 1];
+console.log (firstChar+ ' ' +secondChar+ ' ' +thirdChar+ ' ' +fourthChar+ ' ' +lastChar);
