5205793bbf9ffb72b7b818864e9fcc69c4644441
KaterynaOsadchenko <kattarina88@gmail.com>
2024-02-25 02:43:46 +0200

    init HW_6

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Task 1
+
+const stringOne = prompt ("Enter your name");
+const stringTwo = prompt ("Enter your surname");
+const stringThree = prompt('Enter your favorite color');
+console.log (stringOne + stringTwo + stringThree);
+
+// Task 2
