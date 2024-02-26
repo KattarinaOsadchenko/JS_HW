f44941bf8f690ee7d9c9723a4e36bf376cd6690c
KaterynaOsadchenko <kattarina88@gmail.com>
2024-02-26 19:05:15 +0200

    update new HW_6

--- js/script.js
+++ js/script.js
@@ -5,7 +5,17 @@
 const name = prompt ("Enter your name");
 const surname = prompt ("Enter your surname");
 const color = prompt('Enter your favorite color');
-const сoncatenation = name + '' + surname + '' + color;
-console.log (сoncatenation);
+const сoncPhrase = name + '' + surname + '' + color;
+console.log (сoncPhrase);
 
 // Task 2
+
+const number = 12345;
+const numberString = number.toString();
+
+const firstChar = numberString [0];
+const secondChar = numberString [1];
+const thirdChar = numberString [2];
+const fourthChar = numberString [3];
+const lastChar = numberString [numberString.length - 1];
+console.log (firstChar+ ' ' +secondChar+ ' ' +thirdChar+ ' ' +fourthChar+ ' ' +lastChar);
