c29c4d01488171f45b1e303c8b62ab3f6e1fc77d
KaterynaOsadchenko <kattarina88@gmail.com>
2024-03-04 00:35:24 +0200

    init HW_11

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 'use strict';
+
+const action= prompt('Enter the mathematical action you would like to perform: if add - enter add, if subtraction - enter sub, if multiplication -enter mult, if division - enter div');
+const numberFirst = +prompt('Enter the first number to perform operation');
+const numberSecond = +prompt('Enter the second number to perform operation');
