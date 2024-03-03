8d7c4bdc829efef4d855c08e7b8e284658341233
KaterynaOsadchenko <kattarina88@gmail.com>
2024-03-03 23:43:30 +0200

    init HW_9

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const temperature = +prompt('Enter the temperature without the unit')
+const markDegree = prompt('Enter the unit of measurement temperature')
+
+if (!isNaN(temperature)) {
+
+    let result = null;
+    if ( markDegree.toUpperCase()==='F') {
+        result = ('It will be ')+(temperature - 32) * 5/9 + ('degrees Celsius');
+    }
+    else if (markDegree.toUpperCase()==='C') {
+        result = ('It will be ')+((temperature*9/5)+32) + ('degrees Fahrenheit');
+    }
+    else {
+        result = ('Error, check the entered data and try again');
+    }
+    alert(result);
+}
+else {
+    alert('Wrong number')
+}
