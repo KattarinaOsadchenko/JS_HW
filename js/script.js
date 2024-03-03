41aaab813ea32e1854dbd4256cd9f8b436f15cdd
KaterynaOsadchenko <kattarina88@gmail.com>
2024-03-04 00:26:23 +0200

    init HW_10

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let numberOne = +prompt('Enter first number');
+let numberTwo = +prompt('Enter second number');
+
+const sum = numberOne + numberTwo;
+console.log('amount:', sum);
+const difference = numberOne - numberTwo;
+console.log('difference:', difference);
+const multiplication = numberOne * numberTwo;
+console.log('multiplication result:', multiplication);
+const division = numberOne/numberTwo;
+console.log('division result:', division);
