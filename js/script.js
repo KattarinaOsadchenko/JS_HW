73b21deba9f60aa025c0b8521cd8f68b83d081d3
KaterynaOsadchenko <kattarina88@gmail.com>
2024-03-10 22:56:25 +0200

    completed HW_16

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+
+const numberOne = +prompt('Enter first number');
+const numberTwo = +prompt('Enter second number');
+const numberThree = +prompt('Enter third number');
+
+const average = (numberOne+numberTwo+numberThree)/3;
+
+alert (average);
