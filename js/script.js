3335dedb1c4d1219b8dfdf13301561c30d299af9
KaterynaOsadchenko <kattarina88@gmail.com>
2024-03-11 17:18:15 +0000

    init HW_20

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// Task 1
+
+const x = 10;
+const y = 7;
+const compare = x>y ? console.log ('x більше за y') : console.log ('x не більше, ніж y');
+
+// Task 3
+
+const num = prompt('Enter number')
+console.log (num.length)
+if (num<0) {
+    console.log('number'+num+'negative')
+} else {
+console.log('number'+num+'positive')
+}
