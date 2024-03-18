2b12339647ea5435df6e15398a67f108b795fc37
KaterynaOsadchenko <kattarina88@gmail.com>
2024-03-18 10:07:33 +0000

    init HW_24

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Task 1
+
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i = 0; i<arr.length; i++) {
+    sum += arr[i];
+}
+
+console.log('сумa елементів arr', sum);
+
+// Task 2
+let square = 0;
+
+for (let i =1; i < arr.length; i++) {
+    square += arr[i] ** 2;
+}
+
+console.log('сума квадратів arr', square)
