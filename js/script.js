5dda8d75037660663d790f148037a2873c528e49
KaterynaOsadchenko <kattarina88@gmail.com>
2024-03-20 09:50:55 +0000

    update HW_19

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 'use strict';
 
-const age = prompt('Enter your age, only number')
+const age = +prompt('Введіть свій вік, лише цифри')
+let word;
+if (!isNaN(age)) {
 
+    if (age % 10 === 1 && age % 100 !== 11) {
+        word = "рік";
+    } else
+        if ((age % 10 >= 2 && age % 10 <= 4) && (age % 100 < 10 || age % 100 >= 20)) {
+        word = "роки";
+    } else {
+        word = "років";
+    }
+}
+alert(age + " " + word);
