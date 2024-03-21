40c33d74bad3bf597a311e679d23d51a4edc924a
KaterynaOsadchenko <kattarina88@gmail.com>
2024-03-21 17:38:12 +0000

    init HW_21

--- js/script.js
+++ js/script.js
@@ -1,1 +1,78 @@
 'use strict';
+
+// 1
+let a= 0;
+const u = (a === 0) ? console.log('Вірно') :  console.log('Неправильно');
+
+// 2
+const c = (a > 0) ? console.log('Вірно') :  console.log('Неправильно');
+
+// 3
+const  d = (a < 0) ? console.log('Вірно') :  console.log('Неправильно');
+
+// 4
+const e = (a >= 0) ? console.log('Вірно') :  console.log('Неправильно');
+
+// 5
+const f = (a <= 0) ? console.log('Вірно') :  console.log('Неправильно');
+
+// 6
+const g = (a !== 0) ? console.log('Вірно') :  console.log('Неправильно');
+
+// 7
+const h = (a === 'test') ? console.log('Вірно') :  console.log('Неправильно');
+
+
+// 8
+const i = (a === '1') ? console.log('Вірно') :  console.log('Неправильно');
+
+// 9
+const comparison = (a>0 && a<5) ? console.log('Вірно') :  console.log('Неправильно');
+
+// 10
+if (a===0 || a===2){
+    a += 7;
+} else {
+    a /= 10;
+}
+console.log(a);
+
+// 11
+let b = 4
+let sum;
+let difference;
+if (a <= 1 && b >= 3 ) {
+    sum = a + b;
+    console.log(sum);
+} else {
+    difference = a - b;
+    console.log(difference);
+}
+
+// 12
+
+let score = ((a > 2 && a < 11) || (b >= 6 && b < 14)) ? 'Вірно' : 'Невірно';
+console.log(score);
+
+// 13
+
+let num = 2
+let result;
+
+switch (num){
+    case 1:
+        result = "зима";
+        break;
+    case 2:
+        result = "весна";
+        break;
+    case 3:
+        result = "літо";
+        break;
+    case 4:
+        result = "осінь";
+        break;
+    default:
+        result = "Недійсне значення";
+}
+console.log(result)
