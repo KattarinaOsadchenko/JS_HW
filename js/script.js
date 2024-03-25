56c843a9d0d368b66ad7584658d93a75b10fa959
KaterynaOsadchenko <kattarina88@gmail.com>
2024-03-25 10:34:04 +0000

    update task 1,2 HW_23

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+// task 1
+
+let str = '';
+
+for (let i = 20; i<=30; i+=0,5) {
+    str += i + ' ';
+}
+console.log(str);
+
+// task 2
+
+let dollar = '';
+
+for (let i = 10; i <= 100; i += 10) {
+    dollar *= 27 + ' ';
+}
+console.log(dollar);
